f2725d29d007cedb7049e9043611023d3a623382
Gabriel Bira <gabriel@littledata.io>
2019-03-01 10:30:42 +0200

    WIP

--- imports/api/fileUploads/server/fileUploads.js
+++ imports/api/fileUploads/server/fileUploads.js
@@ -2,11 +2,20 @@ import { WebApp } from 'meteor/webapp'
 import readline from 'readline'
 import csvParse from 'csv-parse'
 
+const HEADER_VALS = {
+	Castigator: 'supplier',
+}
 //using interal webapp or iron:router
 WebApp.connectHandlers.use('/uploadFile', function (req, res) {
 	const rl = readline.createInterface({
 		input: req,
 		terminal: false,
+	})
+
+	const parser = csvParse({
+		delimiter: '^',
+		skip_lines_with_error: true,
+		columns: headers => headers.map(header => HEADER_VALS[header] || header),
 	})
 
 	rl.on('close', function () {
